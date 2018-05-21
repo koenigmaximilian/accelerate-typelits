419cf130701a2509f2a94fa9140f5111df62e1db
Maximilian König <m.koenig@bit.sv>
2018-05-21 08:25:30 +0200

    edit

--- src/Data/Array/Accelerate/TypeLits/Internal.hs
+++ src/Data/Array/Accelerate/TypeLits/Internal.hs
@@ -143,7 +143,7 @@ instance forall m n. (KnownNat m, KnownNat n) => AccApply (AccMatrix m n) where
 -}
 class AccMean a where
   mean ::
-       forall b c. (Elt b, Num (Exp b), Real b, Elt c, Fractional c)
+       forall b c. (Elt b, Real (Exp b), Elt c, Fractional c)
     => a b
     -> AccScalar Double
 
